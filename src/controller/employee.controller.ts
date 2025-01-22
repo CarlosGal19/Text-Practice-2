a0ca32e2719dee05a4a7cf74821a7d968900d075
Carlos Galindo <utm22030587@utma.edu.mx>
2025-01-22 17:05:49 -0600

    refactor: add try catch implementation

--- src/controller/employee.controller.ts
+++ src/controller/employee.controller.ts
@@ -1,14 +1,18 @@
 import Employee from "../schema/employee.model";
 
 async function addEmployee() {
-    await Employee.create({
-        name: 'Carlos',
-        lastName: 'Galindo',
-        salary: 20000,
-        position: 'Boss',
-        holiday: 12
-    });
-    console.log('Employee added successfully');
+    try {
+        await Employee.create({
+            name: 'Carlos',
+            lastName: 'Galindo',
+            salary: 20000,
+            position: 'Boss',
+            holiday: 12
+        });
+        console.log('Employee added successfully');
+    } catch (error: any) {
+        throw new Error(error.message)
+    }
 }
 
 export {
--- src/controller/supervisor.controller.ts
+++ src/controller/supervisor.controller.ts
@@ -1,14 +1,18 @@
 import Supervisor from "../schema/supervisor.model";
 
 async function addSupervisor() {
-    await Supervisor.create({
-        name: 'Carlos',
-        lastName: 'Galindo',
-        salary: 20000,
-        position: 'Boss',
-        department: 'HH RR'
-    });
-    console.log('Supervisor added successfully');
+    try {
+        await Supervisor.create({
+            name: 'Carlos',
+            lastName: 'Galindo',
+            salary: 20000,
+            position: 'Boss',
+            department: 'HH RR'
+        });
+        console.log('Supervisor added successfully');
+    } catch (error: any) {
+        throw new Error(error.message)
+    }
 }
 
 export {

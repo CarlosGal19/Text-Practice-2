849ac5d468c798fa13cbb04147f4314ee60be689
Carlos Galindo <utm22030587@utma.edu.mx>
2025-01-22 17:01:23 -0600

    feat: add entrypoint

--- index.ts
+++ index.ts
@@ -1,1 +1,25 @@
-console.log('Hola')
+import express from 'express'
+import cors from 'cors'
+import dotenv from 'dotenv'
+
+import connectDB from './src/db/db';
+
+import { addSupervisor } from './src/controller/supervisor.controller';
+import { addEmployee } from './src/controller/employee.controller';
+
+const app = express();
+
+app.use(cors())
+
+dotenv.config();
+
+connectDB();
+
+const PORT = process.env.PORT || 5001
+
+addSupervisor();
+addEmployee();
+
+app.listen(PORT, () => {
+    console.log(`App is running on port: ${PORT}`)
+})
